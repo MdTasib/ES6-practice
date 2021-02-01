03d8fa10c184ec4d9da766054e42c231f909f2ed
ohidul alam <mdtasib442@gmali.com>
2021-02-01 17:33:07 +0600

    es6 practice

---
+++ script.js
@@ -0,0 +1,28 @@
+// function use
+// 1
+// function doubleIt(number) {
+//     return number * 2;
+// }
+// 2
+// const doubleIt = function (number) {
+//     return number * 2;
+// }
+// 3
+// const doubleIt = function myFunc(number) {
+//     return number * 2;
+// }
+// 4
+// const doubleIt = number => number * 2;
+// console.log(doubleIt(2)); // 4
+
+// 5
+// const add = (x, y) => x + y;
+// console.log(add(5, 5)) // 10
+// 6
+// const add = (x, y) => {
+//     const sum = x + y;
+//     const sub = x - y;
+//     const result = sum * sub;
+//     return result;
+// }
+// console.log(add(5, 5)); // 0
