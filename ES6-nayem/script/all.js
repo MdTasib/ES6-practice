bca5b27432fe741cbf6302215809328823786824
ohidul alam <mdtasib442@gmali.com>
2021-02-06 14:11:11 +0600

    es6 practice

---
+++ ES6-nayem/script/all.js
@@ -0,0 +1,9 @@
+// // Template String in Javascript
+// let name = 'OH Tasib';
+// let age = 19;
+
+// console.log(`My name is ${name} and i am ${age} years old..`);
+// console.log(`I am ${age < 18 ? 'not ' : ''}adult`);
+// console.log(name.padStart(10, '*'));
+// console.log(name.padEnd(10, '/'));
+// console.log(`${name.repeat(10)}`);
