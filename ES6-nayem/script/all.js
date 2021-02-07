b2207e0cdcda71b5147f40e517b70e82576b2e00
ohidul alam <mdtasib442@gmali.com>
2021-02-07 21:54:37 +0600

    practice

--- ES6-nayem/script/all.js
+++ ES6-nayem/script/all.js
@@ -7,3 +7,20 @@
 // console.log(name.padStart(10, '*'));
 // console.log(name.padEnd(10, '/'));
 // console.log(`${name.repeat(10)}`);
+
+// // spread operator useing Object and Array
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// const arrSum = arr.reduce((a, b) => a + b);
+// console.log(arr);
+// console.log(...arr);
+// console.log(arrSum);
+
+// let obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3,
+//     d: 4
+// }
+// console.log(obj);
+// let obj2 = { ...obj };
+// console.log(obj2);
