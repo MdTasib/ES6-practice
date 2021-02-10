9cc515d79a104316692192618c613a25554fd78e
ohidul alam <mdtasib442@gmali.com>
2021-02-10 09:23:21 +0600

    practice

--- es6/all.js
+++ es6/all.js
@@ -128,3 +128,29 @@
 // const [...allFriends] = friends;
 // console.log(allFriends);
 // console.log(...allFriends);
+
+// build ES6 Iterators
+// function range(start, end, step) {
+//     let current = start;
+//     return {
+//         [Symbol.iterator]: function () {
+//             return {
+//                 next() {
+//                     let result;
+//                     if (current <= end) {
+//                         result = {
+//                             done: false,
+//                             value: current
+//                         }
+//                         current += step;
+//                         return result;
+//                     };
+//                     return {
+//                         done: true,
+//                     };
+//                 }
+//             }
+//         }
+//     }
+// }
+// console.log([...range(2, 30000, 3)])
